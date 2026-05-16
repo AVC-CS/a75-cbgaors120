8e374731609a48524bceaa1718645e613623f429
cbgaors120 <bgaor@students.avc.edu>
2026-05-15 22:26:20 -0700

    a75 assignment

--- main.hpp
+++ main.hpp
@@ -12,6 +12,19 @@ int finduserstring(char[], int, char[], int);
 int finduserstring(char cstr[], int cstrlen, char userstr[], int userlen)
 {
     // TODO: scan cstr for the first occurrence of userstr, return its starting
+    for (int i=0;i<=cstrlen-userlen;i++){
+        bool findString = true;
+        
+        for (int j=0;j<userlen;j++){
+            if (cstr[i+j] != userstr[j]){
+                findString = false;
+                break;
+            }
+        }
+        if (findString == true)
+            return i;
+    }
+    return -1;
 }
 
 #endif
